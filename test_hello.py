7bbf720d5da317fe771e3779575fa4adfab18967
David Brillinger <brillingerdavid@gmail.com>
2023-08-25 14:15:52 +0200

    Added unit test script

---
+++ test_hello.py
@@ -0,0 +1,20 @@
+import unittest
+from hello import FileWriter
+
+class TestWriteToFile(unittest.TestCase):
+    
+    def test_writing_to_file(self):
+        file_path = "hello.txt"
+        msg = "Hello, X-NET!"
+
+        myfilewriter = FileWriter(file_path)
+
+        myfilewriter.write(msg)
+
+        with open(file_path, "r") as f:
+            content = f.read()
+            expected_content = msg
+            self.assertEqual(content, expected_content)
+
+if __name__ == "__main__":
+    unittest.main()
